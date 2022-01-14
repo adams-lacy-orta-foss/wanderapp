1bdd9f3e216b8a8420f93410c9aa635ed16a40f1
Nick Adams <nick.pappas.adams@gmail.com>
2022-01-14 14:26:53 -0600

    added db seeders

---
+++ DbSeeds/userSeeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(first_name, last_name, user_name, email, phone_number, password, dob, bio)
+VALUES('Nick', 'Adams', 'thebigdeal916', 'thebigdeal916@icloud.com', '916-996-1799', 'test', '11-23-1981', 'test bio here'),
+        ('Joanna', 'Foss', 'Jfoss', 'jfoss@somemail.com', '123-456-7890', 'testing', '12-25-1996', 'another test bio here');
