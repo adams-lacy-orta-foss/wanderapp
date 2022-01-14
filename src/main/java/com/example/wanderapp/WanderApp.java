25f5b8dc3fb9a2129ae65029bdb5c8b22e73736a
Joanna <m.joanna.foss@gmail.com>
2022-01-14 09:59:11 -0600

    Initial commit

---
+++ src/main/java/com/example/wanderapp/WanderApp.java
@@ -0,0 +1,13 @@
+package com.example.wanderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wanderapp/WanderAppTests.java
@@ -0,0 +1,13 @@
+package com.example.wanderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
