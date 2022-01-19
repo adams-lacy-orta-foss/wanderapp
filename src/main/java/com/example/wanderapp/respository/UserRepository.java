42b4815a3d019b4661b3cb45873f980914a036b9
Steven Lacy <steven.lacy11@gmail.com>
2022-01-19 12:09:53 -0600

    Database can be displayed to profile/{username} and displays the Users first name.

--- src/main/java/com/example/wanderapp/respository/UserRepository.java
+++ src/main/java/com/example/wanderapp/respository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.wanderapp.respository;
 import com.example.wanderapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository <User, Long> {
+
+	Object findByUserName(String username);
 }
