392a5758cb26d12fb078a5faf8a244bd9c1a8836
Joanna <m.joanna.foss@gmail.com>
2022-01-20 11:50:56 -0600

    added slash to showLoginForm return

--- src/main/java/com/example/wanderapp/controllers/AuthenticationController.java
+++ src/main/java/com/example/wanderapp/controllers/AuthenticationController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthenticationController {
 	@GetMapping("/login")
-	public String showLoginForm(){return "login";}
+	public String showLoginForm(){return "/login";}
 }
