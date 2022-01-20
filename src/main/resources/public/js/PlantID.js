46593a336f3308e40002e14be474979978b698a1
Kevin Orta <orta.kevin@gmail.com>
2022-01-20 14:03:16 -0600

    working on plant api

---
+++ src/main/resources/public/js/PlantID.js
@@ -0,0 +1,51 @@
+document.querySelector('button').onclick = function sendIdentification() {
+    const files = [...document.querySelector('input[type=file]').files];
+    const promises = files.map((file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const res = event.target.result;
+                console.log(res);
+                resolve(res);
+            }
+            reader.readAsDataURL(file)
+        })
+    })
+
+    Promise.all(promises).then((base64files) => {
+        console.log(base64files)
+
+        const data = {
+            api_key: plantIDAPI,
+            images: base64files,
+            // modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
+            modifiers: ["crops_fast", "similar_images", "health_all", "disease_similar_images"],
+            plant_language: "en",
+            // plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
+            plant_details: ["common_names",
+                "url",
+                "name_authority",
+                "wiki_description",
+                "taxonomy",
+                "synonyms"],
+            // disease details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Disease-details
+            disease_details: ["common_names", "url", "description"]
+        };
+
+        fetch('https://api.plant.id/v2/identify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    })
+
+};
