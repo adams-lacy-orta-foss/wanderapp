0dae4b4ebe8905f6eea5ed7438909f5dca8fc643
Kevin Orta <orta.kevin@gmail.com>
2022-01-24 16:13:44 -0600

    map flies to search

--- src/main/resources/public/js/backend.js
+++ src/main/resources/public/js/backend.js
@@ -2,12 +2,12 @@
 
 $(document).ready(() => {
     let isMobile = false;
-    if( $("#is-mobile-container").css('display') === 'none'){
+    if ($("#is-mobile-container").css('display') === 'none') {
         isMobile = true;
     }
 
     //THIS IS CONDITIONALLY RUN CODE DEPENDING ON WHETHER OR NOT THE USER IS ON A MOBILE DEVICE.
-    if(isMobile){
+    if (isMobile) {
 
     } else {
 
@@ -22,5 +22,19 @@ let map = new mapboxgl.Map(
         style: 'mapbox://styles/mapbox/outdoors-v11',
         center: [-98.4861, 29.4252],
         zoom: 12
+    });
+
+document.getElementById('state-city-btn').addEventListener('click', function (e) {
+        e.preventDefault();
+        let streetInp = document.getElementById('street').value;
+        let cityInp = document.getElementById('city').value;
+        let stateInp = document.getElementById('state').value;
+        let searchInp = streetInp + ' ' + cityInp + ' ' + stateInp;
+
+        geocode(searchInp, mapBoxAPIkey).then(function (data) {
+            console.log(data);
+            map.flyTo({center: data, zoom: 13});
+            let marker = new mapboxgl.Marker().setLngLat(data).addTo(map);
+        });
     }
 );
