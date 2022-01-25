a97f6c12ecfaa4761e6a0c82bf731e670d163cf4
Kevin Orta <orta.kevin@gmail.com>
2022-01-25 13:07:07 -0600

    playing with mapbox

--- src/main/resources/public/js/backend.js
+++ src/main/resources/public/js/backend.js
@@ -16,21 +16,61 @@ $(document).ready(() => {
 
 //INIT MAPBOX
 mapboxgl.accessToken = mapBoxAPIkey;
-const bounds = [
-    [-106.320405, 25.404477, -90.664887, 33.257889]
-];
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.032, 38.913]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'Washington, D.C.'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-122.414, 37.776]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'San Francisco, California'
+            }
+        }
+    ]
+};
 
-const bound2 = [
-    [-100.433890,28.344755],[-96.478812,30.310532]
-];
 let map = new mapboxgl.Map(
     {
         container: "map",
         style: 'mapbox://styles/mapbox/outdoors-v11',
         center: [-98.4861, 29.4252],
         zoom: 12,
-        bbox: bound2
-        });
+    });
+
+
+
+// add markers to map
+for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(feature.geometry.coordinates)
+        .setPopup(
+            new mapboxgl.Popup({offset: 25}) // add popups
+                .setHTML(
+                    `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                )
+        )
+        .addTo(map);
+}
 
 document.getElementById('state-city-btn').addEventListener('click', function (e) {
         e.preventDefault();
@@ -42,7 +82,9 @@ document.getElementById('state-city-btn').addEventListener('click', function (e)
         geocode(searchInp, mapBoxAPIkey).then(function (data) {
             console.log(data);
             map.flyTo({center: data, zoom: 13});
-            let marker = new mapboxgl.Marker().setLngLat(data).addTo(map);
+            // let marker = new mapboxgl.Marker().setLngLat(data).addTo(map);
         });
     }
 );
+
+
