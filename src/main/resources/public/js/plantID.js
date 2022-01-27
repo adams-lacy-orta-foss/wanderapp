4d74d8cb9a58b03000e2bf2d968937ff6a133ae7
Joanna <m.joanna.foss@gmail.com>
2022-01-27 09:25:24 -0600

    some edits

--- src/main/resources/public/js/plantID.js
+++ src/main/resources/public/js/plantID.js
@@ -1,69 +1,71 @@
-document.querySelector('button').onclick = function sendIdentification() {
-    const files = [...document.querySelector('input[type=file]').files];
-    const promises = files.map((file) => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                const res = event.target.result;
-                console.log(res);
-                resolve(res);
-            }
-            reader.readAsDataURL(file)
-        })
-    })
-
-    Promise.all(promises).then((base64files) => {
-        console.log(base64files)
-
-        const data = {
-            api_key: plantIDAPI,
-            images: base64files,
-            // modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
-            modifiers: ["crops_fast", "similar_images"],
-            plant_language: "en",
-            // plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
-            plant_details: ["common_names",
-                "url",
-                "name_authority",
-                "wiki_description",
-                "taxonomy",
-                "synonyms"]
-            // disease details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Disease-details
-            //disease_details: ["common_names", "url", "description"]
-        };
-
-        fetch('https://api.plant.id/v2/identify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                document.getElementById("plant").append(
-                    data.suggestions[0].plant_details.common_names[0]
-                )
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    })
-};
-
-// const webcamElement = document.getElementById('webcam');
-// const canvasElement = document.getElementById('canvas');
-// const snapSoundElement = document.getElementById('snapSound');
-// const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
-
-// webcam.start()
-//    .then(result =>{
-//       console.log("webcam started");
-//    })
-//    .catch(err => {
-//        console.log(err);
-//    });
+// document.querySelector('button').onclick = function sendIdentification() {
+//     const files = [...document.querySelector('input[type=file]').files];
+//     const promises = files.map((file) => {
+//         return new Promise((resolve, reject) => {
+//             const reader = new FileReader();
+//             reader.onload = (event) => {
+//                 const res = event.target.result;
+//                 console.log(res);
+//                 resolve(res);
+//             }
+//             reader.readAsDataURL(file)
+//         })
+//     })
 //
-// webcam.stop();
-// let picture = webcam.snap();
+//     Promise.all(promises).then((base64files) => {
+//         console.log(base64files)
+//
+//         const data = {
+//             api_key: plantIDAPI,
+//             images: base64files,
+//             // modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
+//             modifiers: ["crops_fast", "similar_images"],
+//             plant_language: "en",
+//             // plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
+//             plant_details: ["common_names",
+//                 "url",
+//                 "name_authority",
+//                 "wiki_description",
+//                 "taxonomy",
+//                 "synonyms"]
+//             // disease details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Disease-details
+//             //disease_details: ["common_names", "url", "description"]
+//         };
+//
+//         fetch('https://api.plant.id/v2/identify', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(data),
+//         })
+//             .then(response => response.json())
+//             .then(data => {
+//                 console.log('Success:', data);
+//                 document.getElementById("plant").append(
+//                     data.suggestions[0].plant_details.common_names[0]
+//                 )
+//             })
+//             .catch((error) => {
+//                 console.error('Error:', error);
+//             });
+//     })
+// };
+
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+webcam.start()
+   .then(result =>{
+      console.log("webcam started");
+   })
+   .catch(err => {
+       console.log(err);
+   });
+
+
+let picture = webcam.snap();
+
+webcam.stop();
