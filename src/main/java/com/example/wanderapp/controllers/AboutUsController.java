95a30414401053bee770202683a29242bfa255da
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-05 16:36:58 -0600

    added auth to about us controller

--- src/main/java/com/example/wanderapp/controllers/AboutUsController.java
+++ src/main/java/com/example/wanderapp/controllers/AboutUsController.java
@@ -1,14 +1,28 @@
 package com.example.wanderapp.controllers;
 
 
+import com.example.wanderapp.model.User;
+import com.example.wanderapp.respository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutUsController {
 
+    private final UserRepository userDao;
+
+    public AboutUsController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/about-us")
-    public String aboutUs() {
+    public String aboutUs(Model model) {
+        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.findById(loginUser.getId());
+        model.addAttribute("user", user.getBio());
         return "about-us";
+
     }
 }
